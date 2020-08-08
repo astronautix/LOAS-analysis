11c8bf40abf9466a08f428d2b73b7a960b31538c
titulebolide <seneztit@gmail.com>
2020-08-08 17:55:04 +0200

    improve viewer

--- utils/view.py
+++ utils/view.py
@@ -4,9 +4,19 @@ import loas
 import numpy as np
 import math
 
-def show(mesh, Qs):
+def show(mesh, Qs = None):
+    """
+    if Qs is None, will display the original situation of the satellite
+    if Qs is a single Quaternion, will display the satellite in this situation
+    if Qs is a list, wil display step by step every situations
+    """
+    if Qs is None:
+        Qs = [loas.Quat(1,0,0,0)]
+    elif type(Qs) != list:
+        Qs = [Qs]
     bounds = np.array(mesh.bounds)
     mesh.apply_translation(-(bounds[0] + bounds[1])/2)
+    mesh.apply_scale(1/np.linalg.norm(mesh.extents)) # auto resize
     satellite = vp.compound([
         vp.triangle(
             vs = [
@@ -20,7 +30,7 @@ def show(mesh, Qs):
         vp.arrow(pos=vp.vector(0, 0, 0), axis=axis, shaftwidth=0.01, color=axis)
         for axis in (vp.vector(1,0,0), vp.vector(0,1,0), vp.vector(0,0,1))
     ]]) # add frame to the satellite
-    wind = vp.arrow(pos=vp.vector(0, 0, -3), axis=vp.vector(0, 0, 1), shaftwidth=0.01, color=vp.vector(1, 1, 1))
+    wind = vp.arrow(pos=vp.vector(0, 0, 3), axis=vp.vector(0, 0, -1), shaftwidth=0.01, color=vp.vector(1, 1, 1))
     prevQ = None
     for Q in Qs:
         if prevQ is not None:
